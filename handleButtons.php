e3ece4c34309142a03fafd8c3aa330aeb701cca0
Kelly Bielaski <knbielaski@yahoo.com>
2016-09-30 11:12:27 -0700

    Update handleButtons.php

--- handleButtons.php
+++ handleButtons.php
@@ -2,18 +2,34 @@
 class handleButtons{
 
   private $query;
+  private $dataset;
   private $matrix;
   private $colNames;
 
   public function __construct(){
+    //default matrix of just an empty three columned table
     $this->matrix = array();
     $this->colNames = array("1" => "1", "2" => "2", "3" =>"3");
   }
 
+  public function getMatrix(){
+    return $this->matrix;
+  }
+  function createMatrix(){
+    $this->colNames = array("first", "20", "three", "004", "last"); 
+    $this->matrix = array(
+			  array(1,2,3,4,5),
+			  array(6,7,8,9,10),
+			  array(11,12,13,14,15),
+			  array(16,17,18,19,20),
+			  array(21,22,23,24,25)
+			  );
+    $this->query = "test";
+    $this->dataset = "test";
+
+  }
+
   function makeHTMLTable(){
- 
-    /*
-    //requires that the $matrix and $colNames exist
     echo '<!DOCTYPE html>
 <html>
 <title>Secure Search Front End Tool</title>
@@ -25,41 +41,74 @@ table {
     border-collapse: collapse;
     width: 100%;
 }
-
-td, th {
+td {
     border: 1px solid #dddddd;
     text-align: left;
     padding: 8px;
 }
-
+th {
+    border: 1px solid #dddddd;
+    text-align: center;
+    padding: 0px;
+}
 tr:nth-child(even) {
     background-color: #dddddd;
 }
+}
 </style>
-</head>';
-    //create the buttons
+</head>' . "\n";
+    //dropdown menu of the choices for what happens when you click on a column
+    echo ' <form action= "searchResults.php" method = "get">' . "\n";
+    echo '  <select name="selectColSort" method="get"> 
+   <option value="asc">ascending</option>
+   <option value="dec">descending</option>
+   <option value="delete">delete column</option>
+  </select>' . "\n";
+    //button to do a new search
+    //echo ' <form action= "searchResults.php" method = "get">' . "\n";
+
+    echo '<br>' . "\n";
+    echo '<br>';
     echo "\n<body>\n<table>\n";
     echo "  <tr>\n";
     foreach($this->colNames as $name){
-	echo '    <th>' . $name . '</th>' . "\n";
-      }
+      echo '    <th>' . "\n";
+      echo '     <input type="submit" name="sortBy" value="' . $name . '">' . "\n";
+      echo '     </form>' . "\n";
+      echo '    </th>' . "\n";
+    }
     echo "  </tr>\n";
-   foreach($this->matrix as $aline){
-     echo "  <tr>\n";
-	foreach($aline as $box){
-	  echo '    <td>' . $box . '</td>' . "\n";
-	}
-	echo "  </tr>\n";
-	}
-	echo '</table>' . "\n";
-      echo'</body>
-</html>';*/
+    foreach($this->matrix as $row){
+      echo "  <tr>\n";
+      foreach($row as $entry){
+	echo '   <td>' . $entry . "</td>\n";
+      }
+      echo "  </tr>\n";
+    }
+    echo '</table>' . "\n";
+    echo'</body>
+</html>';
+  }
+  function deleteCol($name){
+    $index = array_search($name, $this->colNames);
+    /*for($x=0; $x<count($this->colNames)-1; $x++){
+      if($this->colNames[$x] == $name){
+      $index = $x;
+      }
+      }*/
+    unset($this->colNames[$index]);
+    $newMatrix = array();
+    foreach($this->matrix as $row){
+      unset($row[$index]);
+      array_push($newMatrix, $row);
+    }
+    $this->matrix = $newMatrix;
   }
 
-  function reOrderMatrix($columnNum, $lth){
+  function reOrderMatrix($colNum, $order){
 
     //if lth is set sort low to high, if not, sort high to low
-    if($lth == "1"){
+    if($order == "1"){
       //I don't know how to sort by column
       //asort($this->matrix, function($a, $b){ return $a[$columnNum]-$b[$columnNum]});
     }
@@ -73,7 +122,12 @@ tr:nth-child(even) {
     //makes the beginning of the table
     //requires that the $matrix and $colNames exist
     $this->query = $displayValue;
-     echo '<!DOCTYPE html>
+
+    //add at the top a box where it says what happens when you click on the button, either, you can get rid of the column because it is all zeros, or you can sort from dec or ascending order
+    //make sure the other types of queries work
+    //aquaint has to work
+    //when you click on the docid you get doc
+    echo '<!DOCTYPE html>
 <html>
 <title>Secure Search Front End Tool</title>
 <h1 style="font-size:300%;text-align:center;">Secure Search Results: ' . $this->query . '</h1>
@@ -84,27 +138,38 @@ table {
     border-collapse: collapse;
     width: 100%;
 }
-
-td, th {
+td {
     border: 1px solid #dddddd;
     text-align: left;
     padding: 8px;
 }
-
+th {
+    border: 1px solid #dddddd;
+    text-align: center;
+    padding: 0px;
+}
 tr:nth-child(even) {
     background-color: #dddddd;
 }
+}
 </style>
-</head>';
-    //create the buttons
+</head>' . "\n";
+    echo ' <form action= "searchResults.php" method = "get">' . "\n";
+    echo '  <select name="selectColSort" method="get">
+   <option value="asc">ascending</option>
+   <option value="dec">descending</option>
+   <option value="delete">delete column</option>
+  </select>' . "\n";
+    echo '<br>'. "\n";
+    echo '<br>' . "\n";
     echo "\n<body>\n<table>\n";
     echo "  <tr>\n";
 
     //these if statements are to select the correct dataset for the preCalculated Search
  
     //make sure index_search and parser are up to date
-    $index_search = '/home/kellybielaski/data/SecureSearch/indexer/index_search';
-    $parser = '/home/kellybielaski/data/SecureSearch/indexer/parser';
+    $index_search = '/var/www/cgi-bin/index_search';
+    $parser = '/var/www/cgi-bin/parser';
     $doclocation = '/silo/datasets/indices';
 
     $preCalc = "T/F";
@@ -122,40 +187,48 @@ tr:nth-child(even) {
 
     //when it is not a preCalculated Query
     if($preCalcDropdown=="default"){
-      $dataset = $dataset;
-      $preCalc = "false";
+      $this->dataset = $dataset;
 
       unset($this->colNames);
       $this->colNames = array("docid", "qid", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33");   //get better descriptors for each of the columns 
       foreach($this->colNames as $name){
-	echo '    <th>' . $name . '</th>' . "\n";
+     	echo '    <th>' . "\n";
+	echo '     <input type="submit" name="sortBy" value="' . $name . '">' . "\n";
+	echo '     </form>' . "\n";
+	echo '    </th>' . "\n";
+
+	//echo '    <th>' . $name . '</th>' . "\n";
       }
-    echo "  </tr>\n";
+      echo "  </tr>\n";
       unset($this->matrix);
       $this->matrix = array();
 
-      $filename = shell_exec("echo " . $this->query . " | " . $index_search . " --index /silo/datasets/indices/" . $dataset . "/index | " . $parser . " " . $this->query . " " . $dataset ." 0");
-      //$f_res = fopen("/tmp/filesa22tV", "r");
+      $filename = shell_exec("echo " . $this->query . " | " . $index_search . " --index /silo/datasets/indices/" . $this->dataset . "/index | " . $parser . " " . $this->query . " " . $this->dataset ." 0");
+      if($filename == NULL){
+	echo "problem with shell_exec";
+      }
+      
       $filename = str_replace("\n", '', $filename);
       $f_res = fopen($filename, "r");
-      //while(!feof($f_res)){
+      while(!feof($f_res)){
 	$line = fgets($f_res);
-	echo $line;
-	$arrOfLine = explode(" ",$line);
-	echo "  <tr>\n";
-	array_pop($arrOfLine);
-	$docid = $arrOfLine[sizeof($arrOfLine)-1];
-	array_unshift($arrOfLine, $docid);
-	array_pop($arrOfLine);
-	foreach($arrOfLine as $stat){
-	  echo '   <td>' . $stat . '</td>' . "\n";
+	if ($line != ""){
+	  $arrOfLine = explode(" ",$line);
+	  echo "  <tr>\n";
+	  array_pop($arrOfLine);
+	  $docid = $arrOfLine[sizeof($arrOfLine)-1];
+	  array_unshift($arrOfLine, $docid);
+	  array_pop($arrOfLine);
+	  foreach($arrOfLine as $stat){
+	    echo '   <td>' . $stat . '</td>' . "\n";
+	  }
+	  echo "  </tr>\n";
+	  array_push($this->matrix, $arrOfLine);
 	}
-	fclose($f_res);
-	unlink($filename);
-	echo "  </tr>\n";
-	array_push($this->matrix, $arrOfLine);
-	//}
-	array_pop($this->matrix);
+      }
+      array_pop($this->matrix);
+      fclose($f_res);
+      unlink($filename);
     }
 
 
@@ -201,30 +274,44 @@ tr:nth-child(even) {
 
 
 
-
-$handler = new handleButtons();
-
 //I am using these values to test
-$_GET["preCalcDropdown"] = "default";
+
+/*$_GET["selectColSort"] = "delete";
+  $_GET["sortBy"] = "three";*/
+
+/*$_GET["preCalcDropdown"] = "default";
 $_GET["displayValue"] = "hello";
-$_GET["dataset"] = "trec45";
+$_GET["dataset"] = "trec08";*/
+
 
 //the preCalculated Queries don't work yet
+
+
 if(isset($_GET["preCalcDropdown"]) and isset($_GET["displayValue"]) and isset($_GET["dataset"])){
-  /*echo $_GET["preCalcDropdown"];
-  echo $_GET["displayValue"];
-  echo $_GET["dataset"];*/
-  $handler->submitQuery($_GET["preCalcDropdown"], $_GET["displayValue"], $_GET["dataset"]);
-
+  $matrix = new handleButtons();
+  $matrix->submitQuery($_GET["preCalcDropdown"], $_GET["displayValue"], $_GET["dataset"]);
 }
 
-//if one of the columns buttons are set, then change the matrix and then print the new matrix
-if(isset($_GET["col"]) and isset($_GET["lth"])){
-  $handler->reOrderMatrix($_GET["col"], $_GET["lth"]);
-  $handler->makeHTMLTable();
+if(isset($_GET["selectColSort"]) and isset($_GET["sortBy"])){
+    //$matrix->createMatrix();
+    if($_GET["selectColSort"] == "delete"){
+      $matrix->deleteCol($_GET["sortBy"]);
+    }
+    if($_GET["selectColSort"] == "asc"){
+      $matrix->reOrderMatrix($_GET["sortBy"]);
+    }
+    if($_GET["selectColSort"] == "delete"){
+      $matrix->reOrderMatrix($_GET["sortBy"]);
+    }
+    $matrix->makeHTMLTable();
+  }
+
+
+else{
+  echo "ERROR: all search fields are not filled out";
 }
 
-//also handle press to the button that has a link to 
+
 
 
 ?>
